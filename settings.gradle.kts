2313a0987b229d69ead95ab6fe19d37347b05ecc
Dmytro Primshyts <dprimshyts@gmail.com>
2018-03-04 18:06:47 +0200

    Detekt IntelliJ Plugin bootstrap

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "detekt"
+include("detekt-api",
+		"detekt-core",
+		"detekt-rules",
+		"detekt-cli",
+		"detekt-test",
+		"detekt-sample-extensions",
+		"detekt-gradle-plugin",
+		"detekt-generator",
+		"detekt-watch-service",
+		"detekt-intellij-plugin")
+
