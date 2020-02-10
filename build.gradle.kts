970a0217b0f8e016a32ab3788961fecfc143499e
Artur Bosch <arturbosch@gmx.de>
2020-02-10 23:41:04 +0100

    Use implementation instead of deprecated compile configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,11 @@ plugins {
 }
 
 dependencies {
-	compile("io.gitlab.arturbosch.detekt:detekt-api:$detektVersion")
-	compile("io.gitlab.arturbosch.detekt:detekt-core:$detektVersion")
-	compile("io.gitlab.arturbosch.detekt:detekt-cli:$detektVersion")
-	compile("io.gitlab.arturbosch.detekt:detekt-rules:$detektVersion")
-	compile("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
+	implementation("io.gitlab.arturbosch.detekt:detekt-api:$detektVersion")
+	implementation("io.gitlab.arturbosch.detekt:detekt-core:$detektVersion")
+	implementation("io.gitlab.arturbosch.detekt:detekt-cli:$detektVersion")
+	implementation("io.gitlab.arturbosch.detekt:detekt-rules:$detektVersion")
+	implementation("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
 }
 
 tasks.withType<KotlinCompile> {
