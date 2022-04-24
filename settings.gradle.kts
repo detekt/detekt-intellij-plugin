8d8f83c2068b094066358131084348eb7eac7ec9
Chao Zhang <chao.zhang@instacart.com>
2022-04-23 17:52:16 -0700

    Use Gradle enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,30 @@
+import com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+
 rootProject.name = "detekt-intellij-plugin"
+
+// build scan plugin can only be applied in settings file
+plugins {
+    `gradle-enterprise`
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.6.5"
+}
+
+gradleEnterprise {
+    val isCiBuild = System.getenv("CI") != null
+
+    buildScan {
+        publishAlways()
+
+        // Publish to scans.gradle.com when `--scan` is used explicitly
+        if (!gradle.startParameter.isBuildScan) {
+            server = "https://ge.detekt.dev"
+            this as BuildScanExtensionWithHiddenFeatures
+            publishIfAuthenticated()
+        }
+
+        isUploadInBackground = !isCiBuild
+
+        capture {
+            isTaskInputFiles = true
+        }
+    }
+}
