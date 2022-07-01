48a18ab2fc1c8cf9fcc5d9059b74bacd924a96ee
Chao Zhang <chao.zhang@instacart.com>
2022-07-01 11:43:05 -0700

    Add Gradle enterprise access keys

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ rootProject.name = "detekt-intellij-plugin"
 // build scan plugin can only be applied in settings file
 plugins {
     `gradle-enterprise`
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.6.5"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.7.2"
 }
 
 gradleEnterprise {
