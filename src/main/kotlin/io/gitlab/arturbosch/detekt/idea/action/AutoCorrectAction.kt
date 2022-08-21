9dee6d6a0582fd64de229a60e31b258a59dec100
Artur Bosch <abosch@posteo.de>
2022-08-21 19:36:46 +0200

    Log errors to debug if stack traces without detekt mentioned are thrown by our plugin (#268)
    * Log errors to try debug if errors without detekt stacktraces are thrown by our plugin
    
    * Do not allow autocorrect action on readonly files like kt files inside jars
    
    * Prepare 1.21.2 release

--- src/main/kotlin/io/gitlab/arturbosch/detekt/idea/action/AutoCorrectAction.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/idea/action/AutoCorrectAction.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
@@ -16,12 +17,14 @@ import io.gitlab.arturbosch.detekt.idea.util.showNotification
 
 class AutoCorrectAction : AnAction() {
 
+    private val logger = logger<AutoCorrectAction>()
+
     override fun update(event: AnActionEvent) {
         val file: VirtualFile = event.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
         val project = event.getData(CommonDataKeys.PROJECT) ?: return
 
-        if (file.extension in KOTLIN_FILE_EXTENSIONS) {
-            // enable auto correct option only when plugin is enabled
+        if (!file.fileSystem.isReadOnly && file.extension in KOTLIN_FILE_EXTENSIONS) {
+            // enable autocorrect option only when plugin is enabled
             event.presentation.isEnabledAndVisible = project.isDetektEnabled()
         } else {
             // hide action for non-Kotlin source files
@@ -33,7 +36,13 @@ class AutoCorrectAction : AnAction() {
         val virtualFile = event.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
         val project = event.getData(CommonDataKeys.PROJECT) ?: return
         val psiFile = PsiManager.getInstance(project).findFile(virtualFile) ?: return
-        runAction(project, psiFile)
+        if (virtualFile.fileSystem.isReadOnly) {
+            logger.trace("Skipping readOnly file: ${virtualFile.path}")
+            return
+        }
+        runCatching { runAction(project, psiFile) }
+            .onFailure { logger.error("Unexpected error while performing auto correct action", it) }
+            .getOrThrow()
     }
 
     internal fun runAction(project: Project, psiFile: PsiFile) {
