faa0ab4a3c9458a2f5db6021399d6105bb4953ec
Artur Bosch <abosch@posteo.de>
2023-02-20 15:06:42 +0100

    Make NewDetektConfigUi the default one without provider interface

--- src/main/kotlin/io/gitlab/arturbosch/detekt/idea/config/ui/DetektConfigUiProvider.kt
+++
@@ -1,8 +0,0 @@
-package io.gitlab.arturbosch.detekt.idea.config.ui
-
-import com.intellij.openapi.ui.DialogPanel
-
-internal interface DetektConfigUiProvider {
-
-    fun createPanel(): DialogPanel
-}
