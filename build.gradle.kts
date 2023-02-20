10ef283c59f6557164153ec2bb2de1a2419e3d69
Artur Bosch <abosch@posteo.de>
2023-02-20 14:07:22 +0100

    Update versions to verify
    Update minimal IJ platform version to 222.4459.24

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,13 +68,13 @@ tasks.publishPlugin {
 
 intellij {
     pluginName.set("Detekt IntelliJ Plugin")
-    version.set("2022.2.3")
+    version.set("2022.2.4")
     updateSinceUntilBuild.set(false)
     plugins.set(listOf("IntelliLang", "Kotlin"))
 }
 
 tasks.runPluginVerifier {
-    ideVersions.set(listOf("2022.1.4", "2022.2.3"))
+    ideVersions.set(listOf("2022.2.4", "2022.3.2"))
     failureLevel.set(listOf(DEPRECATED_API_USAGES, INVALID_PLUGIN))
 }
 
