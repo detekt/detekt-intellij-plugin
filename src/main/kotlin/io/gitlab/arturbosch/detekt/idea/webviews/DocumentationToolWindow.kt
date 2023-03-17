bc103944397915cefb00e43a34370d1385b53f88
Artur Bosch <abosch@posteo.de>
2023-03-17 21:09:30 +0100

    Show fallback message in case JCEF is not available

--- src/main/kotlin/io/gitlab/arturbosch/detekt/idea/webviews/DocumentationToolWindow.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/idea/webviews/DocumentationToolWindow.kt
@@ -4,6 +4,8 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBPanelWithEmptyText
+import com.intellij.ui.jcef.JBCefApp
 
 class DocumentationToolWindow : ToolWindowFactory, DumbAware {
 
@@ -16,10 +18,19 @@ class DocumentationToolWindow : ToolWindowFactory, DumbAware {
     }
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val browser = RulesBrowserService.getInstance()
-        val content = toolWindow.contentManager.factory
-            .createContent(browser.getComponent(), null, false)
-        content.setDisposer(browser)
+        val content = if (JBCefApp.isSupported()) {
+            val browser = RulesBrowserService.getInstance()
+            toolWindow.contentManager.factory
+                .createContent(browser.getComponent(), null, false)
+                .apply { setDisposer(browser) }
+        } else {
+            val component = JBPanelWithEmptyText().apply {
+                emptyText.text = "JCEF is not supported. Please make sure to use Jetbrains JDK."
+            }
+            toolWindow.contentManager.factory
+                .createContent(component, null, false)
+        }
+
         toolWindow.contentManager.addContent(content)
     }
 }
