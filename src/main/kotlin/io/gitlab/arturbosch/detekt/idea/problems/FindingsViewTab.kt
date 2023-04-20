7cb047ee8fc58c8374f64df9df70ca649f312509
Artur Bosch <abosch@posteo.de>
2023-04-20 21:02:39 +0200

    Select problems view detekt tab on run finish

--- src/main/kotlin/io/gitlab/arturbosch/detekt/idea/problems/FindingsViewTab.kt
+++ src/main/kotlin/io/gitlab/arturbosch/detekt/idea/problems/FindingsViewTab.kt
@@ -2,21 +2,30 @@ package io.gitlab.arturbosch.detekt.idea.problems
 
 import com.intellij.analysis.problemsView.toolWindow.ProblemsViewPanel
 import com.intellij.analysis.problemsView.toolWindow.ProblemsViewState
+import com.intellij.analysis.problemsView.toolWindow.ProblemsViewToolWindowUtils
+import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
 import io.gitlab.arturbosch.detekt.idea.DetektBundle
+
+private const val FINDINGS_TAB_ID = "io.github.arturbosch.detekt.idea.findings.tab"
 
 class FindingsViewTab(
     project: Project,
     state: ProblemsViewState,
 ) : ProblemsViewPanel(
     project,
-    "io.github.arturbosch.detekt.idea.findings.tab",
+    FINDINGS_TAB_ID,
     state,
     DetektBundle.lazy("detekt.configuration.title"),
 ) {
 
     init {
         treeModel.root = FindingsRootNode(project, this)
-        FindingsManager.getInstance().register { treeModel.structureChanged(null) }
+        FindingsManager.getInstance().register {
+            invokeLater {
+                treeModel.structureChanged(null)
+                ProblemsViewToolWindowUtils.selectTab(project, FINDINGS_TAB_ID)
+            }
+        }
     }
 }
